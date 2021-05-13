63fc39f92a7a85f8d87eaf34ed62195962b97863
greatdawid <45632821+greatdawid@users.noreply.github.com>
2021-05-13 10:16:03 +0200

    questions

--- experiments.py
+++ experiments.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.callbacks import CSVLogger
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import RepeatedStratifiedKFold
 import numpy as np
 import matplotlib.pyplot as plt
 import dataset_split as ds
@@ -21,13 +22,17 @@ import os
 import cv2 as cv
 import pandas as pd
 
-EPOCH = 5
 BATCH_SIZE = 16
 
 classes = ["without_mask", "with_mask"]
 labels = []
 data = []
-
+#img = cv.imread('00088.png')
+#img = cv.resize(img, (1024, 1024))
+#img = img[1762:2554, 870:1662]
+#cv.imshow('testwin',img)# scale to dimension
+#cv.waitKey(0)
+#exit(0)
 # def prepare_data_and_labels_():
 pan = pd.read_json('kaggle_dataset.json')
 df = pd.DataFrame(pan)
@@ -70,11 +75,12 @@ for idx, image in enumerate(os.listdir(ds.PATH_TO_IMAGES)):
                 except:
                     pass
 
+
+
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 lb = LabelEncoder()
 labels = lb.fit_transform(labels)
-labels = to_categorical(labels)
 
 aug = ImageDataGenerator(
     zoom_range=0.1,
@@ -86,28 +92,37 @@ aug = ImageDataGenerator(
     fill_mode="nearest"
 )
 
+#docelowo na wejsciu dwa zbiory, jeden z rzeczywistymi maseczkami drugi z sztucznymi
 
 rs = 10
-n_splits = 5
-n_repeats = 2
-#strafified
-rkf = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state =rs)
+n_splits = 2
+n_repeats = 5
+rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state =rs)
+predictions = []
+true_labels = []
+NUM_EPOCHS = 10
+for fold_id, (train_index, test_index) in enumerate(rskf.split(data, labels)):
 
-for fold_id, (train_index, test_index) in enumerate(rkf.split(data)):
-    print("Fold Number: %s", fold_id)
+    print(f"Fold Number: {fold_id}")
+    #labels = to_categorical(labels, dtype="int32")
     X_train, X_test = data[train_index], data[test_index]
-    Y_train, Y_test = labels[train_index], labels[test_index]
+    Y_train, Y_test = to_categorical(labels[train_index], dtype="int32"),\
+                      to_categorical(labels[test_index], dtype="int32")
+    #Y_train, Y_test = labels[train_index], labels[test_index]
     baseModel = MobileNetV2(weights="imagenet", include_top=False,
                             input_tensor=Input(shape=(224, 224, 3)))
     headModel = baseModel.output
-    headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+    headModel = AveragePooling2D(pool_size=(5, 5))(headModel)
     headModel = Flatten(name="flatten")(headModel)
     headModel = Dense(64, activation="relu")(headModel)
     headModel = Dropout(0.5)(headModel)
     headModel = Dense(2, activation="softmax")(headModel)
 
+#  !!Jak ma wyglądać strategia dla odmrazania? Czy np dla kazdego folda mam robic
+    #  kolejnego fora z ilościa odmrozonych warstw? Ilość, 1-10?
+    #!! Jak duża ma być ilość odmrożonych warstw?
+    #zakladam, że po prostu odmrażam kolejne warstwy bez wzgledu jakie są
     model = Model(inputs=baseModel.input, outputs=headModel)
-
     for layer in baseModel.layers:
         layer.trainable = False
 
@@ -120,15 +135,27 @@ for fold_id, (train_index, test_index) in enumerate(rkf.split(data)):
     print("[INFO] training head...")
     H = model.fit(
         aug.flow(X_train, Y_train, batch_size=16),
-        steps_per_epoch= len(X_train) // 22,
-        validation_data= (X_test, Y_test),
-        validation_steps= len(X_test) // 22,
+        steps_per_epoch=len(X_train) // 16,
+        validation_data=(X_test, Y_test),
+        validation_steps=len(X_test) // 16,
         callbacks=[csv_logger],
-        epochs=2)
-    #scores = model.evaluate(data[test_index], labels[test_index])
+        epochs=NUM_EPOCHS)
+    #predictions.append(model.predict(X_test)) # czy zrzutowac to na 0 lub 1
+    predicts = model.predict(X_test)
+    predicts_labels = np.argmax(predicts, axis=-1)
+    #print(model.predict(X_test))
+    #print(predicts_labels)
+    true_labels.append(Y_test)
+    #!!w macierzy sa predykcje i etykiety, w csv_logger są wyniki walidacji itp (loss i accurracy)!!
+
     #Y predict aby miec etykiety
     #doloz predyckje i etykiety do macierzy aby miec wszystkow jednym miejsuc
-#model.save("model_1.model", save_format="h5")
+    #!!Czy te predykcje i etykiety maja byc dla kazdego folda, epoki, i odmrozonej warstwy? Wtedy lista 5D lub 4D jesli tylko dla folda !!
+
+#zachowuje model
+#!!czy zachować model dla kazdego foldu?, może tylko dla najlepszej!!?
+
+model.save(f"model_natural.model", save_format="h5")
 
 
 # plt.style.use("ggplot")
