18d0b3cc3685d3559cebf445e2c5ec83b9129488
greatdawid <45632821+greatdawid@users.noreply.github.com>
2021-05-20 08:14:55 +0200

    Petla do skonsultacji

--- experiments.py
+++ experiments.py
@@ -1,3 +1,5 @@
+import random
+
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
@@ -14,7 +16,9 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.callbacks import CSVLogger
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.metrics import accuracy_score
 import numpy as np
 import matplotlib.pyplot as plt
 import dataset_split as ds
@@ -24,58 +28,31 @@ import pandas as pd
 
 BATCH_SIZE = 16
 
-classes = ["without_mask", "with_mask"]
 labels = []
 data = []
-#img = cv.imread('00088.png')
-#img = cv.resize(img, (1024, 1024))
-#img = img[1762:2554, 870:1662]
-#cv.imshow('testwin',img)# scale to dimension
-#cv.waitKey(0)
-#exit(0)
-# def prepare_data_and_labels_():
-pan = pd.read_json('kaggle_dataset.json')
-df = pd.DataFrame(pan)
-# print(ds.PATH_TO_IMAGES)
-# for idx, image in enumerate(os.listdir(ds.PATH_TO_IMAGES)):
-#    X, Y = df["pic_dims"][idx]
-#    img = cv.imread(image)
-#    cv.imshow('w',img)
-#    #cv.resize(img,(200,200))
-#    #cv.resize(img,(int(X),int(Y)))
-#    for face in df.columns[3:]:
-#        info = df[face][idx]
-#        print(info)
-# print("Wczytuje obrazy")
+
+PATH_TO_REAL_DATASET='Datasets/NaturalFaces/Images/'
+PATH_TO_ARTIFICIAL_DATASET='Datasets/ArtificialFaces'
+files = np.array(os.listdir(PATH_TO_ARTIFICIAL_DATASET))
+random.shuffle(files)
+
+NUMBER_OF_FILES = len(files[:40])
 
 print("Extracting each data into respective label folders....")
-for idx, image in enumerate(os.listdir(ds.PATH_TO_IMAGES)):
-    img = cv.imread(os.path.join(ds.PATH_TO_IMAGES, image))
-    # scale to dimension
-    X, Y = df["pic_dims"][idx]
-    cv.resize(img, (int(X), int(Y)))
-    # find the face in each object
-    for face in df.columns[3:]:
-        info = df[face][idx]
-        if info != 0:
-            label = info[0]
-            if (label == "mask_weared_incorrect"):
-                label = 'without_mask'
-                info[0] = 'without_mask'
-            info[0] = info[0].replace(str(label), str(classes.index(label)))
-            info = [int(each) for each in info]
-            face = img[info[2]:info[4], info[1]:info[3]]
-            if ((info[3] - info[1]) > 40 and (info[4] - info[2]) > 40):
-                try:
-                    face = cv.resize(face, (224, 224))
-                    face = img_to_array(face)
-                    face = preprocess_input(face)
-                    data.append(face)
-                    labels.append(label)
-                except:
-                    pass
-
-
+for idx, path in enumerate(files[:40]):
+    label =' '
+    if(path.endswith('Mask.jpg')):
+        label = 'withmask'
+    else:
+        label = "withoutmask"
+    #pre_str = path.split('.')[0].split('_')[-2:]
+    #label = pre_str[0]+pre_str[1]
+    face = cv.imread(os.path.join(PATH_TO_ARTIFICIAL_DATASET, path))
+    face = cv.resize(face, (224, 224))
+    face = img_to_array(face)
+    face = preprocess_input(face)
+    labels.append(label)
+    data.append(face)
 
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
@@ -98,64 +75,73 @@ rs = 10
 n_splits = 2
 n_repeats = 5
 rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state =rs)
-predictions = []
 true_labels = []
-NUM_EPOCHS = 10
-for fold_id, (train_index, test_index) in enumerate(rskf.split(data, labels)):
+NUM_EPOCHS = 1
+NUM_UNFREEZED_LAYERS = 1
 
-    print(f"Fold Number: {fold_id}")
-    #labels = to_categorical(labels, dtype="int32")
-    X_train, X_test = data[train_index], data[test_index]
-    Y_train, Y_test = to_categorical(labels[train_index], dtype="int32"),\
-                      to_categorical(labels[test_index], dtype="int32")
-    #Y_train, Y_test = labels[train_index], labels[test_index]
-    baseModel = MobileNetV2(weights="imagenet", include_top=False,
-                            input_tensor=Input(shape=(224, 224, 3)))
-    headModel = baseModel.output
-    headModel = AveragePooling2D(pool_size=(5, 5))(headModel)
-    headModel = Flatten(name="flatten")(headModel)
-    headModel = Dense(64, activation="relu")(headModel)
-    headModel = Dropout(0.5)(headModel)
-    headModel = Dense(2, activation="softmax")(headModel)
+#macierz z wynikami |WARSTWA|FOLD|ETYKIETA|PREDYKCJA|
+predictions = np.zeros((NUM_UNFREEZED_LAYERS, n_splits * n_repeats, NUMBER_OF_FILES // 4), dtype=int)
+true_labels = np.zeros((NUM_UNFREEZED_LAYERS, n_splits * n_repeats, NUMBER_OF_FILES // 4), dtype=int)
 
-#  !!Jak ma wyglądać strategia dla odmrazania? Czy np dla kazdego folda mam robic
-    #  kolejnego fora z ilościa odmrozonych warstw? Ilość, 1-10?
-    #!! Jak duża ma być ilość odmrożonych warstw?
-    #zakladam, że po prostu odmrażam kolejne warstwy bez wzgledu jakie są
-    model = Model(inputs=baseModel.input, outputs=headModel)
-    for layer in baseModel.layers:
-        layer.trainable = False
 
-    print("[INFO] compiling model...")
-    opt = Adam(lr=1e-4, decay=1e-4 / 10)
-    model.compile(loss="binary_crossentropy", optimizer=opt,
-                  metrics=["accuracy"])
+for index in range(0,NUM_UNFREEZED_LAYERS):
+    print(f"{index}")
+    for fold_id, (train_index, test_index) in enumerate(rskf.split(data, labels)):
 
-    csv_logger = CSVLogger('training_log.csv',append=True)
-    print("[INFO] training head...")
-    H = model.fit(
-        aug.flow(X_train, Y_train, batch_size=16),
-        steps_per_epoch=len(X_train) // 16,
-        validation_data=(X_test, Y_test),
-        validation_steps=len(X_test) // 16,
-        callbacks=[csv_logger],
-        epochs=NUM_EPOCHS)
-    #predictions.append(model.predict(X_test)) # czy zrzutowac to na 0 lub 1
-    predicts = model.predict(X_test)
-    predicts_labels = np.argmax(predicts, axis=-1)
-    #print(model.predict(X_test))
-    #print(predicts_labels)
-    true_labels.append(Y_test)
-    #!!w macierzy sa predykcje i etykiety, w csv_logger są wyniki walidacji itp (loss i accurracy)!!
+        print(f"Fold Number: {fold_id}")
+        #podział na zbio walidacjny i testowy w proporcjach 50|50
+        X_val, X_test, y_val, y_test = train_test_split(data[test_index], labels[test_index],
+                                                               test_size=0.5, random_state=rs)
 
-    #Y predict aby miec etykiety
-    #doloz predyckje i etykiety do macierzy aby miec wszystkow jednym miejsuc
-    #!!Czy te predykcje i etykiety maja byc dla kazdego folda, epoki, i odmrozonej warstwy? Wtedy lista 5D lub 4D jesli tylko dla folda !!
+        #labels = to_categorical(labels, dtype="int32")
+        X_train = data[train_index]
+        Y_train, Y_test, Y_val = to_categorical(labels[train_index], dtype="int32"),\
+                          to_categorical(labels[y_test], dtype="int32"),\
+                          to_categorical(labels[y_val], dtype="int32")
+        #Y_train, Y_test = labels[train_index], labels[test_index]
+        baseModel = MobileNetV2(weights="imagenet", include_top=False,
+                                input_tensor=Input(shape=(224, 224, 3)))
+        headModel = baseModel.output
+        headModel = AveragePooling2D(pool_size=(5, 5))(headModel)
+        headModel = Flatten(name="flatten")(headModel)
+        headModel = Dense(64, activation="relu")(headModel)
+        headModel = Dropout(0.5)(headModel)
+        headModel = Dense(2, activation="sigmoid")(headModel)
 
-#zachowuje model
-#!!czy zachować model dla kazdego foldu?, może tylko dla najlepszej!!?
+        model = Model(inputs=baseModel.input, outputs=headModel)
 
-model.save(f"model_natural.model", save_format="h5")
+        #Tu odmrożonie warstw w zależności od parametru
+        for layer in baseModel.layers[:index]:
+            layer.trainable = False
+
+        print("[INFO] compiling model...")
+        opt = Adam(lr=1e-4, decay=1e-4 / 10)
+        model.compile(loss="binary_crossentropy", optimizer=opt,
+                      metrics=["accuracy"])
+
+        csv_logger = CSVLogger('training_log_1.csv',append=True)
+        print("[INFO] training head...")
+        H = model.fit(
+            aug.flow(X_train, Y_train, batch_size=16),
+            steps_per_epoch=len(X_train) // 16,
+            validation_data=(X_val, Y_val),
+            validation_steps=len(X_val) // 16,
+            callbacks=[csv_logger],
+            epochs=NUM_EPOCHS)
+
+        predicts = model.predict(X_test)
+        predicts_labels = np.argmax(predicts, axis=-1)
+
+        for i in range(len(predicts_labels)):
+            true_labels[index, fold_id, i] = labels[y_test][i]
+            predictions[index, fold_id, i] = predicts_labels[i]
+
+        print(accuracy_score(y_test, predicts_labels))
+        model.save(f"model_{index}.model", save_format="h5")
+
+
+
+
 
 
 # plt.style.use("ggplot")
